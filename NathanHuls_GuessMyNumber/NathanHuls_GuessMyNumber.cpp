d9ff23e8add37bb39bab41f6b1adcd36f5f4271b
NATHULS <NATHULS@uat.edu>
2018-01-25 13:16:44 -0700

    Changed rand max to 64

--- NathanHuls_GuessMyNumber/NathanHuls_GuessMyNumber.cpp
+++ NathanHuls_GuessMyNumber/NathanHuls_GuessMyNumber.cpp
@@ -17,7 +17,7 @@ int main()
 {
 	srand(static_cast<unsigned int>(time(0)));		//seed random number generator
 
-	int secretNumber = rand() % 100 + 1;		//random number between 1 and 64; 
+	int secretNumber = rand() % 64 + 1;		//random number between 1 and 64; 
 	int tries = 0; 
 	int guess; 
 
